18e843920babfb9560a4de00a4ae51969a678dfb
Rahul-7483 <rahulbo628@gmail.com>
2024-05-20 16:42:15 +0530

    Update 26_interrupt.c

--- 26_interrupt.X/26_interrupt.c
+++ 26_interrupt.X/26_interrupt.c
@@ -15,7 +15,7 @@
 #pragma config CPD = OFF        // Data EEPROM Memory Code Protection bit (Data EEPROM code protection off)
 #pragma config WRT = OFF        // Flash Program Memory Write Enable bits (Write protection off; all program memory may be written to by EECON control)
 #pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
-
+//HII RAHUL 
 // #pragma config statements should precede project file includes.
 // Use project enums instead of #define for ON and OFF.
 
